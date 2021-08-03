3a50eb6f8dd68472cf51054c2ca6d6a9d209994a
olili1999 <olili@umich.edu>
2021-08-03 02:04:45 -0400

    added hover, finally figured out sticky, need to fix sticky for small screen sizes

--- src/App.js
+++ src/App.js
@@ -18,11 +18,11 @@ return (
     {/* <Router> */}
     <NavigationBar></NavigationBar>
 
-    <Container fluid style = {{border: "1px solid green"}}> 
+    <Container fluid> 
       <Row style = {{height: "100%"}}> 
 
         {/* Big Outer Column #1 */}
-        <Col xs = {12} sm = {12} md ={3} style = {{height: "100%"}}> 
+        <Col xs = {12} sm = {12} md ={3} style = {{height: "100%", position: "sticky", top: "0px"}}> 
           <Card style={{width: '20rem', margin: '20px 0 20px 20px'}}>
             <Card.Img variant="top" src= {headshot_outside}/>
             <Card.Body>
@@ -33,7 +33,7 @@ return (
             </Card.Body>
           <ListGroup className="list-group-flush">
             <ListGroupItem><FaMapMarker/> Ann Arbor, MI</ListGroupItem>
-            <ListGroupItem><IoLogoLinkedin />&nbsp;<a target = "_blank" href = "https://www.linkedin.com/in/oliver-y-li/">LinkedIn</a></ListGroupItem>
+            <ListGroupItem><IoLogoLinkedin />&nbsp;<a style ={{textDecoration: 'none'}} target = "_blank" href = "https://www.linkedin.com/in/oliver-y-li/">LinkedIn</a></ListGroupItem>
             <ListGroupItem><FaGithub/>&nbsp;<a style ={{textDecoration: 'none'}} target = "_blank"  href ="https://github.com/olili1999">GitHub</a> </ListGroupItem>
             <ListGroupItem><IoDocumentTextSharp/> <a style ={{textDecoration: 'none'}} target = "_blank" href ="https://drive.google.com/file/d/1C3zs45lwpNLw_OIpEd0m0-eV_YEVSUo_/view?usp=sharing">Resume</a> </ListGroupItem>
           </ListGroup>
@@ -42,19 +42,19 @@ return (
         {/* Big Outer Column #2 */}
         <Col xs = {12} sm = {12} md = {9} style = {{}}>
         
-          <h2 style = {{marginTop: "20px"}}> <BsPersonLinesFill/> about me </h2> 
-          <p class = "lead"> Hi, nice to meet you! I'm Oliver, a tennis player, rock climber, programmer, designer, and student at the University of Michigan. Throughout college, I was pretty clueless about what I wanted to do it upon myself to try anything and everything. Over time, I found my initial home in <a style ={{textDecoration: 'none'}}  target= "_blank" href ="https://ioe.engin.umich.edu/">IOE, where I learned that I enjoy streamlining frustratingly inefficient processes</a>. 
+          <h2 id = "about" style = {{marginTop: "20px"}}> <BsPersonLinesFill/> about me </h2> 
+          <p class = "lead"> Hey, nice to meet you! I'm Oliver, a tennis player, rock climber, programmer, designer, and student at the University of Michigan. Throughout college, I was pretty clueless about what I wanted to do it upon myself to try anything and everything. Over time, I found my initial home in <a style ={{textDecoration: 'none'}}  target= "_blank" href ="https://ioe.engin.umich.edu/">IOE, where I learned that I enjoy streamlining frustratingly inefficient processes</a>. 
           After my sophomore year, I did a data science internship at Principal Financial Group, where I learned that I love creating 
-          accessible and intuitive user experiences and making data-driven decisions and <a style ={{textDecoration: 'none'}}  target= "_blank" href = "https://www.si.umich.edu/about-umsi"> decided to take on an additional major through the School of Information in Applied Data Science & User Experience Design. 
-          </a> After gaining some rudimentary programming skills, I realized it was paramount to have a foundation in computer science to have a solid understanding of core engineering principles so that I could build products from the ground up myself.  <a target = "_blank" style ={{textDecoration: 'none'}} href = "https://cse.engin.umich.edu/"> Thus, I pursued a minor in computer science in addition to my dual degree with a concentration in User Interface Design. </a>
+          accessible and intuitive user experiences and making data-driven decisions.  <a style ={{textDecoration: 'none'}}  target= "_blank" href = "https://www.si.umich.edu/about-umsi"> I then decided to take on an additional major through the School of Information in Applied Data Science & User Experience Design. 
+          </a> After gaining some rudimentary programming skills, I realized it was paramount to have a foundation in computer science to have a solid understanding of core engineering principles so that I could build products from the ground up myself.  <a target = "_blank" style ={{textDecoration: 'none'}} href = "https://cse.engin.umich.edu/"> Somehow, I also managed to tack on a minor in computer science in addition to my dual degree with a concentration in User Interface Design. </a>
           My interdisciplinary studies in engineering, business, and design have lead me to be really interested in a career in 
           product management/operations/analytics roles. </p> 
-          <h2 style = {{marginTop: "20px"}}> <FaClipboardCheck/> projects </h2> 
+          <h2 id = "projects" style = {{marginTop: "20px"}}> <FaClipboardCheck/> projects </h2> 
           <p class = "lead"> I enjoy working on a variety of projects both academic and some for fun. Here are a few examples:</p> 
           <Container> 
             <Row> 
               <Col xs = {12} sm = {12} md = {6} lg = {4}> 
-                <Card border="dark" style={{ width: '18rem' }}>
+                <Card className = "bob-on-hover" border="dark" style={{ width: '18rem' }}>
                     <Card.Body>
                       <Card.Title>🍕 discord pizza bot</Card.Title>
                       <Card.Text>
@@ -68,7 +68,7 @@ return (
                 </Card>
               </Col> 
               <Col xs = {12} sm = {12} md = {6} lg = {4}> 
-                <Card border="dark" style={{ width: '18rem' }}>
+                <Card className = "bob-on-hover" border="dark" style={{ width: '18rem' }}>
                     <Card.Body>
                       <Card.Title>🎾 tennis friends </Card.Title>
                       <Card.Text>
@@ -83,7 +83,7 @@ return (
                 </Card>
               </Col>
               <Col xs = {12} sm = {12} md = {6} lg = {4}>
-                <Card border="dark" style={{ width: '18rem' }}>
+                <Card className = "bob-on-hover" border="dark" style={{ width: '18rem' }}>
                     <Card.Body>
                       <Card.Title> 🏭 detroit manufacturing systems </Card.Title>
                       <Card.Text>
@@ -96,7 +96,7 @@ return (
                 </Card>
               </Col> 
               <Col xs = {12} sm = {12} md = {6} lg = {4}>  
-                <Card border="dark" style={{ width: '18rem' }}>
+                <Card className = "bob-on-hover" border="dark" style={{ width: '18rem' }}>
                     <Card.Body>
                       <Card.Title> cover letter generator</Card.Title>
                       <Card.Text>
@@ -112,7 +112,7 @@ return (
               </Col>
             </Row>
           </Container> 
-          <h2 style = {{marginTop: "20px"}}> <MdWork/> work experience </h2> 
+          <h2 id = "work_experience"style = {{marginTop: "20px"}}> <MdWork/> work experience </h2> 
 
 
         </Col>
